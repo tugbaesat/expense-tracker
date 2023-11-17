8310af631ef498726e9f39ede0ada4e3ea6245a4
Tugba Esat <tugbaesat007@gmail.com>
2023-11-17 14:02:58 -0800

    dropdown menu changed

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let menuButton = document.getElementById('menu-button');
-    let dropdownMenu = document.querySelector('.absolute');
-
-    menuButton.addEventListener('click', function() {
-      let isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
-      menuButton.setAttribute('aria-expanded', !isExpanded);
-      dropdownMenu.style.display = isExpanded ? 'none' : 'block';
-    });
-
-    // Close the dropdown menu when clicking outside of it
-    document.addEventListener('click', function(event) {
-      if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
-        menuButton.setAttribute('aria-expanded', 'false');
-        dropdownMenu.style.display = 'none';
-      }
-    });
-  });
+document.addEventListener("DOMContentLoaded", function () {});
